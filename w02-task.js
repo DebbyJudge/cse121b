2c0a906e5157c33fb2e372a8855d143d6d5109f8
DebbyJudge <144963741+DebbyJudge@users.noreply.github.com>
2024-01-18 09:40:40 +0100

    Add files via upload

---
+++ w02-task.js
@@ -0,0 +1,30 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Deborah Judge";
+let currentYear = "2024";
+let profilepicture = "images/profile.jpg"; // Replace with the actual path to your profile picture
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("picture img"); // Assuming you have an img element inside the picture
+const foodElement = document.getElementById("food");
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = "<strong>Deborah Judge</strong>";
+imageElement.setAttribute("src", profilepicture);
+
+/* Step 5 - Array */
+const favouriteFoods = ['Friedrice', 'Chicken pie', 'pasta', 'Toasted bread', 'plantain',]
+foodElement.innerHTML=favouriteFoods
+const favFood = ['Pizza']
+favouriteFoods.push(favFood);
+foodElement.innerHTML += `<br>${favouriteFoods}`;
+favouriteFoods.shift(favFood);
+foodElement.innerHTML += `<br>${favouriteFoods}`;
+favFood.pop();
+foodElement.innerHTML += `<br>${favouriteFoods}`;
+
